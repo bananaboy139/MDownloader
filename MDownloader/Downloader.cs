3e805cf1124995fc8068927909c9efc27d047888
bananaboy139 <matteob319@gmail.com>
2021-06-16 14:07:22 +0200

    Add project files.

---
+++ MDownloader/Downloader.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MDownloader
+{
+    public class Downloader
+    {
+        public void DownloadAsync(DownloadLink L)
+        {
+            //URL to URI
+            Uri URI = new Uri(L.URL);
+            //Get downloads folder path
+
+            if (URI.IsFile)
+            {
+                string filename = Path.GetFileName(URI.LocalPath);
+
+                WebClient Wc = new WebClient()
+                {
+                    Credentials = L.Credentials,
+                    Proxy = L.Proxy
+                };
+                Wc.DownloadProgressChanged += Wc_DownloadProgressChanged;
+                Wc.DownloadFileCompleted += Wc_DownloadFileCompleted;
+                Wc.DownloadFileAsync(URI, L.path + filename);
+            }
+        }
+
+        private void Wc_DownloadFileCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Wc_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class DownloadLink
+    {
+        public string URL;
+
+        public ICredentials Credentials;
+
+        public IWebProxy Proxy;
+
+        public string path = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), 
+            "Downloads");
+    }
+}
---
+++ MDownloader/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using Spectre.Console;
+
+namespace MDownloader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            DownloadLink LINK = new DownloadLink();
+            //get link info
+            LINK.URL = AnsiConsole.Ask<string>("What is the [blue]URL[/]?");
+            if (!AnsiConsole.Confirm("Download to [green]Downloads Folder?[/]"))
+            {
+                LINK.path = Path.GetFullPath(AnsiConsole.Ask<string>("[green]Download path?[/]"));
+            }//missing credentials and proxy
+
+            //Download file
+            Downloader Dow = new Downloader();
+            //fixme: not implemented yet
+            AnsiConsole.Progress()
+                .Start(ctx =>
+                {
+                    // Define tasks
+                    var task1 = ctx.AddTask("[green]Downloading[/]");
+
+                    while (!ctx.IsFinished)
+                    {
+                        task1.Increment(1.5);
+                    }
+                });
+            Dow.DownloadAsync(LINK);
+        }
+    }
+}
